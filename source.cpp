6a3d7b4daa987dab5bd5d3e1063272ff4b99f8ff
epuschel <73982403+epuschel@users.noreply.github.com>
2021-09-27 09:50:08 -0400

    Create source.cpp

---
+++ source.cpp
@@ -0,0 +1,12 @@
+/*
+*Author: Ethan Puschell
+*Creation Date: 9-27-21
+* Modification Date: 9-27-21
+* Purpose: 
+*/
+
+#include<iostream>
+#include<math.h>
+#include<stdlib.h>
+int i = atoi("321");
+
