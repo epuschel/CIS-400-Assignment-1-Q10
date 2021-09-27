264ecb232c508f6d0ae06c7d6b83b029549d7b3d
epuschel <73982403+epuschel@users.noreply.github.com>
2021-09-27 10:09:00 -0400

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -8,7 +8,17 @@
 #include<iostream>
 #include<math.h>
 #include<stdlib.h>
+#include<string>
 #include "Header.h"
 using namespace std;
 int i = atoi("321");
 
+int main()
+{
+	string user_input;
+	cout << "Please enter input for the calculator language (0 1 2 3 4 5 6 7 8 9 + - * /  = )" << endl;
+	cin >> user_input;
+
+	system("pause");
+	return 0;
+}
